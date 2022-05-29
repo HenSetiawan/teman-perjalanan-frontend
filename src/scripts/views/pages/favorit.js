54bc880e9a690f7ab42cab60605451521b72dd65
Ghina <g.salma.susilo@gmail.com>
2022-05-30 06:20:16 +0700

    add folder src

---
+++ src/scripts/views/pages/favorit.js
@@ -0,0 +1,13 @@
+const Favorit = {
+    async render() {
+      return `
+        <h2>Favorit Page</h2>
+      `;
+    },
+   
+    async afterRender() {
+      // Fungsi ini akan dipanggil setelah render()
+    },
+  };
+   
+  export default Favorit;
---
+++ src/scripts/views/pages/koleksi.js
@@ -0,0 +1,13 @@
+const Koleksi = {
+    async render() {
+      return `
+        <h2>Koleksi page</h2>
+      `;
+    },
+   
+    async afterRender() {
+      // Fungsi ini akan dipanggil setelah render()
+    },
+  };
+   
+  export default Koleksi;
