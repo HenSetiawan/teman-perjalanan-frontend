8f363f9d2cc2a10eac6c0eb7a63fd373b041263f
dimmascool <dimasadam058@gmail.com>
2022-05-31 23:46:10 +0700

    Test menambahkan bootstrap via CDN

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,7 @@ import 'regenerator-runtime';
 import '../styles/main.css';
 import App from './views/app';
 
+
 const app = new App({
   //   button: document.querySelector('#hamburgerButton'),
   //   drawer: document.querySelector('#navigationDrawer'),
