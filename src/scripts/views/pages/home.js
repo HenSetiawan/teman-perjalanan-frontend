2d8d142e98e396b85c49654d0afabcf2dc059f60
hensetiawan <hendystyawan74@gmail.com>
2022-06-02 11:52:32 +0700

    add hero section

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -1,13 +1,27 @@
 const Home = {
-    async render() {
-      return `
-        <h2>Home Page</h2>
+  async render() {
+    return `
+      <div class="row mt-5 hero-section">
+      <div class="col-lg-6">
+        <h1 class="mt-5">Mulai <span>perjalanan</span> mengagumkan anda dari sini.</h1>
+        <p class="text-secondary">
+          Telusuri berbagai tempat wisata menarik bersama orang terdekat anda
+        </p>
+      </div>
+      <div class="col-lg-6">
+        <img
+          src="./images/hero.png"
+          alt="traveller"
+          class="img-responsive hero-image"
+        />
+      </div>
+    </div>
       `;
-    },
-   
-    async afterRender() {
-      // Fungsi ini akan dipanggil setelah render()
-    },
-  };
-   
-  export default Home;
+  },
+
+  async afterRender() {
+    // Fungsi ini akan dipanggil setelah render()
+  },
+};
+
+export default Home;
