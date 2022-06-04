b031bac61f5b66a653edf640ed5ca1def3f0c97b
dimmascool <dimasadam058@gmail.com>
2022-06-04 12:23:04 +0700

    menambahkan modal tambah admin dan objek wisata

--- src/scripts/views/pages/data_admin.js
+++ src/scripts/views/pages/data_admin.js
@@ -10,7 +10,9 @@ const dataAdmin = {
               <div class="d-sm-flex align-items-center justify-content-between">
                 <h6 class="m-0 font-weight-bold text-primary">Daftar Administrator</h6>
                 <div class="d-sm-inline-block">
-                    <a class="btn btn-sm btn-primary" href="#">Tambah Administrator</a>
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalTambahAdmin">
+                      Create new administrator
+                    </button>
                 </div>
               </div>                
             </div>
@@ -39,6 +41,46 @@ const dataAdmin = {
             </div>
           </div>
         </div>
+
+        <div class="modal fade bd-example-modal-lg" id="modalTambahAdmin" tabindex="-1" role="dialog" aria-labelledby="labelModalTambahAdmin" aria-hidden="true">
+          <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="labelModalTambahAdmin">Create new administrator</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <form class="row" method="POST" action="">
+                  <div class="col-md-6">
+                      <label for="namaAdmin" class="form-label">Nama</label>
+                      <input type="text" class="form-control" id="namaAdmin" name="nama" required>
+                  </div>
+                  <div class="col-md-6">
+                      <label for="EmailAdmin" class="form-label">Email</label>
+                      <input type="email" class="form-control" id="EmailAdmin" name="email" required>
+                  </div>
+                  <div class="col-md-6">
+                      <label for="usernameAdmin" class="form-label">Username</label>
+                      <input type="text" class="form-control" id="usernameAdmin" name="username" required>
+                  </div>
+                  <div class="col-md-6">
+                      <label for="passwordAdmin" class="form-label">Password</label>
+                      <input type="text" class="form-control" id="passwordAdmin" name="password" required>
+                  </div>               
+                  <div class="col-md-6 mt-3">
+                      <button class="btn btn-outline-primary" name="submit">Kirim Data</button>
+                  </div>
+                </form>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
       `;
   },
 
