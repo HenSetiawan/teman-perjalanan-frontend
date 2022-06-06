530ffb7f44554f3e841892c8d9ba5acdc2ccd82c
Ghina <g.salma.susilo@gmail.com>
2022-06-07 05:57:35 +0700

    add detail page

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,7 +1,37 @@
 const Detail = {
     async render() {
       return `
-        <h2>Detail page</h2>
+        <div class="container">
+          <div class="card travel-card">
+            <img
+              src="./images/hero.png"
+              class="card-img-top"
+              alt="image travel"
+            />
+            <div class="card-body">
+              <h5 class="card-title text-center">
+                Raja Ampat
+              </h5>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">
+                  Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec tristique ut neque ac convallis. Vivamus nec ligula at ipsum iaculis eleifend. Cras venenatis sodales neque eget venenatis. Pellentesque at aliquam risus. Cras dignissim nisl ut mi auctor, vel mollis magna accumsan. Maecenas id nulla vitae ligula pretium pretium. Etiam consequat libero id ligula gravida, at commodo lacus auctor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                </li>
+                <li class="list-group-item">
+                  <strong>kota Raja Ampat</strong>
+                  alamat Raja Ampat
+                </li>
+                <li class="list-group-item">
+                <small class="text-muted">
+                  Last updated 3 mins ago
+                </small>
+                </li>
+              </ul>
+              <p class="card-text text-justify">
+                
+              </p>      
+            </div>
+          </div>
+        </div>
       `;
     },
    
