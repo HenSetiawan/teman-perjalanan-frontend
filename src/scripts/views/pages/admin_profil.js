1bffa1a512ec6a8f0561104d56d676bb3e2a8fe4
dimmascool <dimasadam058@gmail.com>
2022-06-10 19:36:05 +0700

    Menyambungkan data admin profil dengan API

--- src/scripts/views/pages/admin_profil.js
+++ src/scripts/views/pages/admin_profil.js
@@ -1,75 +1,21 @@
 import { getCurrentAdmin } from '../../services/admin-service';
+import { adminProfil } from '../template/template-creator'
 const adminProfile = {
   async render() {
     return `
         <br><center><h2>PROFIL ADMINISTRATOR</h2></center>
         <hr>
 
-        <div class"container-fluid">
-            <div class="row gutters-sm">
-              <div class="col-md-4 mb-3">
-                <div class="card">
-                  <div class="card-body">
-                    <div class="d-flex flex-column align-items-center text-center">
-                      <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" class="rounded-circle" width="150">
-                      <div class="mt-3">
-                        <h4>SamidMada</h4>
-                        <p class="text-secondary mb-1"></p>
-                        <p class="text-muted font-size-sm">-</p><!-- 
-                        <button class="btn btn-primary">Follow</button>
-                        <button class="btn btn-outline-primary">Message</button> -->
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-8">
-                <div class="card mb-3">
-                  <div class="card-body">
-                    <div class="row">
-                      <div class="col-sm-3">
-                        <h6 class="mb-0">Full Name</h6>
-                      </div>
-                      <div class="col-sm-9 text-secondary">Nama User</div>
-                    </div>
-                    <hr>
-                    <div class="row">
-                      <div class="col-sm-3">
-                        <h6 class="mb-0">Email</h6>
-                      </div>
-                      <div class="col-sm-9 text-secondary">UserBlablaBla@gmail.com</div>
-                    </div>
-                    <hr>
-                    <div class="row">
-                      <div class="col-sm-3">
-                        <h6 class="mb-0">Nomor Telpon</h6>
-                      </div>
-                      <div class="col-sm-9 text-secondary">-</div>
-                    </div>
-                    <hr>
-                    <div class="row">
-                      <div class="col-sm-3">
-                        <h6 class="mb-0">Alamat</h6>
-                      </div>
-                      <div class="col-sm-9 text-secondary">-</div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-12">
-                <div class="float-right">
-                    <a href="#/data_admin" class="btn btn-primary">Data Admin</a>
-                    <a href="#/data_objek_wisata" class="btn btn-success">Data Objek Wisata</a>
-                </div>
-              </div>
-            </div>
+        <div class"container-fluid" id="container">
+            
         </div>
       `;
   },
 
   async afterRender() {
     const admin = await getCurrentAdmin();
-    console.log(admin);
+    const container = document.querySelector('#container');
+    container.innerHTML += adminProfil(admin.admin);
   },
 };
 
--- src/scripts/views/template/template-creator.js
+++ src/scripts/views/template/template-creator.js
@@ -84,6 +84,64 @@ const createLikedButtonTemplate = () => `
   </button>
 `;
 
+const adminProfil = (data) => `
+  <div class="row gutters-sm">
+    <div class="col-md-4 mb-3">
+      <div class="card">
+        <div class="card-body">
+          <div class="d-flex flex-column align-items-center text-center">
+            <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" class="rounded-circle" width="150">
+            <div class="mt-3">
+              <h4>${data.name}</h4>
+              <p class="text-secondary mb-1"></p>
+              <p class="text-muted font-size-sm">${data.no_telp}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-8">
+      <div class="card mb-3">
+        <div class="card-body">
+          <div class="row">
+            <div class="col-sm-3">
+              <h6 class="mb-0">Full Name</h6>
+            </div>
+            <div class="col-sm-9 text-secondary">${data.name}</div>
+          </div>
+          <hr>
+          <div class="row">
+            <div class="col-sm-3">
+              <h6 class="mb-0">Email</h6>
+            </div>
+            <div class="col-sm-9 text-secondary">${data.email}</div>
+          </div>
+          <hr>
+          <div class="row">
+            <div class="col-sm-3">
+              <h6 class="mb-0">Nomor Telpon</h6>
+            </div>
+            <div class="col-sm-9 text-secondary">${data.no_telp}</div>
+          </div>
+          <hr>
+          <div class="row">
+            <div class="col-sm-3">
+              <h6 class="mb-0">Alamat</h6>
+            </div>
+            <div class="col-sm-9 text-secondary">${data.alamat}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-12">
+      <div class="float-right">
+          <a href="#/data_admin" class="btn btn-primary">Data Admin</a>
+          <a href="#/data_objek_wisata" class="btn btn-success">Data Objek Wisata</a>
+      </div>
+    </div>
+  </div>
+`
+
 export {
   createCard,
   dataTableWisata,
@@ -91,4 +149,5 @@ export {
   detailDestination,
   createLikeButtonTemplate,
   createLikedButtonTemplate,
+  adminProfil
 };
